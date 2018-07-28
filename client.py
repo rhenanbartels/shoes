ff8de2ed3936db7a62a63f61a00e64f3784e7a62
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-07-28 18:32:59 -0300

    Get users recet media

--- client.py
+++ client.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 from decouple import config
 from InstagramAPI import InstagramAPI
 
@@ -8,3 +10,16 @@ def login():
     api = InstagramAPI(username, passwd)
     api.login()
     return api
+
+
+def get_recent_media(api, users):
+    media = []
+    last_30_min = (
+            datetime.utcnow() - timedelta(minutes=30)
+    ).timestamp()
+    for user in users:
+        media.extend(
+            api.getTotalUserFeed(user['pk'], minTimestamp=last_30_min)
+        )
+
+    return media
