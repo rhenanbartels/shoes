fef1c4dbc40513a5688b68820c803fb86f062694
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-08-02 14:05:16 -0300

    Pass single user as argument to search media

--- commands.py
+++ commands.py
@@ -2,33 +2,31 @@ from client_google_visual import visual_api
 from client_instagram import get_recent_media, get_stories
 
 
-def update_users(db_document, users, origin):
+def update_users(db_collection, users, origin):
     for user in users:
         pk = user['pk']
         user['origin'] = origin
-        db_document.find_one_and_update(
+        db_collection.find_one_and_update(
                 {'pk': pk}, {'$set': user}, upsert=True
         )
 
 
-def search_feed_media(api, db_document, users, delta=1440):
-    for user in users:
-        medias = get_recent_media(api, user, delta)
-        for media in medias:
-            # Check if media contains a photo
-            if 'image_versions2' in media:
-                is_target = visual_api()
-                if is_target:
-                    media['source'] = 'feed'
-                    db_document.insert_one(media)
+def search_feed_media(api, db_collection, user, delta=1440):
+    medias = get_recent_media(api, user, delta)
+    for media in medias:
+        # Check if media contains a photo
+        if 'image_versions2' in media:
+            is_target = visual_api()
+            if is_target:
+                media['source'] = 'feed'
+                db_collection.insert_one(media)
 
 
-def search_stories(api, db_document, users):
-    for user in users:
-        stories = get_stories(api, user)
-        for storie in stories['items']:
-            if storie['media_type'] == 1:
-                is_target = visual_api()
-                if is_target:
-                    storie['source'] = 'story'
-                    db_document.insert_one(storie)
+def search_stories(api, db_collection, user):
+    stories = get_stories(api, user)
+    for storie in stories['items']:
+        if storie['media_type'] == 1:
+            is_target = visual_api()
+            if is_target:
+                storie['source'] = 'story'
+                db_collection.insert_one(storie)
--- tests_commands.py
+++ tests_commands.py
@@ -62,46 +62,34 @@ def test_save_followers():
                 },
                 upsert=True)
     ]
-    document_mock.find_one_and_update.assert_has_calls(calls)
+    collection_mock.find_one_and_update.assert_has_calls(calls)
 
 
 @mock.patch('commands.visual_api')
 @mock.patch('commands.get_recent_media')
 def test_save_media(_get_recent_media, _visual_api):
-    user1 = {'username': 'username1', 'pk': 1234},
-    user2 = {'username': 'username2', 'pk': 5678}
-    followers = [user1, user2]
+    user = {'username': 'username1', 'pk': 1234},
 
-    _visual_api.side_effect = [True, True, True, True]
+    _visual_api.side_effect = [True, False]
 
-    expected_media_1 = deepcopy(media_resp_1)
-    expected_media_2 = deepcopy(media_resp_2)
-    expected_media_1[0]['source'] = 'feed'
-    expected_media_2[0]['source'] = 'feed'
-    expected_media_2[1]['source'] = 'feed'
+    expected_media = deepcopy(media_resp_1)
+    expected_media[0]['source'] = 'feed'
 
     _get_recent_media.side_effect = [media_resp_1, media_resp_2]
     api_mock = mock.MagicMock()
-    media_calls = [mock.call(api_mock, user1, 1440),
-                   mock.call(api_mock, user2, 1440)]
-    document_db_calls = [
-            mock.call(expected_media_1[0]),
-            mock.call(expected_media_2[0]),
-            mock.call(expected_media_2[1])
-    ]
 
-    document_mock = mock.MagicMock()
+    collection_mock = mock.MagicMock()
 
-    search_feed_media(api_mock, document_mock, followers)
+    search_feed_media(api_mock, collection_mock, user)
 
-    _get_recent_media.assert_has_calls(media_calls)
-    document_mock.insert_one.assert_has_calls(document_db_calls)
+    _get_recent_media.assert_called_once_with(api_mock, user, 1440)
+    collection_mock.insert_one.assert_called_once_with(expected_media[0])
 
 
 @mock.patch('commands.visual_api')
 @mock.patch('commands.get_recent_media')
 def test_save_media_only_with_shoes(_get_recent_media, _visual_api):
-    users = [{'username': 'username1', 'pk': 1234}]
+    user = {'username': 'username1', 'pk': 1234}
 
     _visual_api.side_effect = [True, False]
 
@@ -111,45 +99,31 @@ def test_save_media_only_with_shoes(_get_recent_media, _visual_api):
 
     _get_recent_media.side_effect = [media_resp_1, media_resp_2]
     api_mock = mock.MagicMock()
-    media_calls = [mock.call(api_mock, users[0], 1440)]
+    media_calls = [mock.call(api_mock, user, 1440)]
 
-    document_mock = mock.MagicMock()
+    collection_mock = mock.MagicMock()
 
-    search_feed_media(api_mock, document_mock, users)
+    search_feed_media(api_mock, collection_mock, user)
 
     _get_recent_media.assert_has_calls(media_calls)
-    document_mock.insert_one.assert_called_once_with(expected_media_1[0])
+    collection_mock.insert_one.assert_called_once_with(expected_media_1[0])
 
 
 @mock.patch('commands.visual_api')
 @mock.patch('commands.get_stories')
 def test_save_media_from_stories(_get_stories, _visual_api):
-    users = [
-        {'username': 'username1', 'pk': 1234},
-        {'username': 'username1', 'pk': 5678}
-    ]
+    user = {'username': 'username1', 'pk': 1234}
 
     api_mock = mock.MagicMock()
     _get_stories.side_effect = [storie_resp_1, storie_resp_2]
-    document_mock = mock.MagicMock()
+    collection_mock = mock.MagicMock()
 
-    expected_storie_1 = deepcopy(storie_resp_1)
-    expected_storie_2 = deepcopy(storie_resp_2)
-    expected_storie_1['items'][0]['source'] = 'story'
-    expected_storie_2['items'][0]['source'] = 'story'
-    expected_storie_2['items'][1]['source'] = 'story'
+    expected_storie = deepcopy(storie_resp_1)
+    expected_storie['items'][0]['source'] = 'story'
 
-    stories_calls = [
-            mock.call(api_mock, users[0]),
-            mock.call(api_mock, users[1])
-    ]
-    document_db_calls = [
-            mock.call(expected_storie_1['items'][0]),
-            mock.call(expected_storie_2['items'][0]),
-            mock.call(expected_storie_2['items'][1])
-    ]
+    search_stories(api_mock, collection_mock, user)
 
-    search_stories(api_mock, document_mock, users)
-
-    _get_stories.assert_has_calls(stories_calls)
-    document_mock.insert_one.assert_has_calls(document_db_calls)
+    _get_stories.assert_called_once_with(api_mock, user)
+    collection_mock.insert_one.assert_called_once_with(
+            expected_storie['items'][0]
+    )
