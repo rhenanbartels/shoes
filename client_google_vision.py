751f637acfd7a263d6e9eb36cb73926bb469c2e5
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-08-05 16:53:43 -0300

    Get labels from google vision api

--- client_google_vision.py
+++ client_google_vision.py
@@ -1,13 +1,44 @@
+import base64
+import requests
+
 from decouple import config, Csv
 
 
 TARGET_KEYWORDS = config('TARGET_KEYWORDS', cast=Csv())
+API_VERSION = config('VISION_API_VERSION')
+TOKEN = config('VISION_TOKEN')
 
 
-def vision_api():
-    return True
+def vision_api(image_url):
+    response = requests.get(image_url)
+    base64_image = _prepare_image(response)
+    return find_keywords(get_identified_labels(base64_image))
+
+
+def get_identified_labels(base64_image):
+    url = 'https://vision.googleapis.com/{api_version}/images:annotate?'\
+          'key={token}'.format(api_version=API_VERSION, token=TOKEN)
+    body = {
+        'requests': [{
+            'image': {
+                'content': base64_image,
+            },
+            'features': [{
+                'type': 'LABEL_DETECTION',
+                'maxResults': 5,
+            }]
+
+        }]
+    }
+    header = {'Content-Type': 'application/json'}
+    response = requests.post(url, headers=header, json=body)
+    return response.json()['responses'][0]
 
 
 def find_keywords(json_response):
     keys = json_response.get('labelAnnotations', [])
     return any([key['description'] in TARGET_KEYWORDS for key in keys])
+
+
+def _prepare_image(response):
+    return base64.b64encode(response.content).decode()
--- tests_client_google.py
+++ tests_client_google.py
@@ -1,14 +1,46 @@
+import base64
+
+from unittest import mock
+
 import responses
 
 from decouple import config
 
-from client_google_vision import find_keywords
+from client_google_vision import find_keywords, get_identified_labels
 from fixtures import vision_api_non_target, vision_api_target
 
 
 def test_find_target_keywords():
-    not_target = find_keywords(visual_api_non_target)
-    target = find_keywords(visual_api_target)
+    not_target = find_keywords(vision_api_non_target['responses'][0])
+    target = find_keywords(vision_api_target['responses'][0])
 
     assert not_target is False
     assert target is True
+
+
+@responses.activate
+def test_get_google_vision_api_response():
+    api_version = config('VISION_API_VERSION')
+    token = config("VISION_TOKEN")
+
+    with open('test_images/vision_test_image.jpg', 'rb') as image:
+        image_content = image.read()
+        base64_image = base64.b64encode(image_content).decode()
+
+    responses.add(
+            responses.POST,
+            "https://vision.googleapis.com/{api_version}/images:annotate?"
+            "key={token}".format(api_version=api_version, token=token),
+            json=vision_api_non_target,
+            status=200
+            )
+
+    labels = get_identified_labels(base64_image)
+
+    assert labels == vision_api_non_target['responses'][0]
+
+
+@mock.patch('client_google_vision.get_identified_labels')
+@mock.patch('client_google_vision.requests.get')
+def test_vision_api_use(_get, _get_labels):
+    pass
