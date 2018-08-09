0086a3c3503b3e5523eaf73b732568e22ebc58b0
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-08-09 16:24:32 -0300

    Add paginator for users and media

--- core/views.py
+++ core/views.py
@@ -1,18 +1,35 @@
+from django.core.paginator import Paginator
 from django.http import JsonResponse
 from django.views import View
 
 from core.models import client
 
 
+N_USER_PAGE = 2
+N_MEDIA_PAGE = 2
+
+
 class UsersView(View):
     def get(self, request, *args, **kwargs):
+        page_num = int(request.GET.get('page', 1))
+
         db_users = client.shoes.users
         users = list(db_users.find({}, {'_id': 0}))
-        return JsonResponse(users, safe=False)
+
+        paginator = Paginator(users, N_USER_PAGE)
+        page = paginator.page(page_num)
+
+        return JsonResponse(page.object_list, safe=False)
 
 
 class FeedView(View):
     def get(self, request, *args, **kwargs):
-        db_users = client.shoes.media
-        users = list(db_users.find({}, {'_id': 0}))
-        return JsonResponse(users, safe=False)
+        page_num = int(request.GET.get('page', 1))
+
+        db_media = client.shoes.media
+        media = list(db_media.find({}, {'_id': 0}))
+
+        paginator = Paginator(media, N_MEDIA_PAGE)
+        page = paginator.page(page_num)
+
+        return JsonResponse(page.object_list, safe=False)
