99762f9f282653aa715c716d57b60ce2cec538b0
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-08-08 23:05:16 -0300

    Enhance jobs code

--- jobs.py
+++ jobs.py
@@ -2,29 +2,35 @@ import argparse
 
 from datetime import datetime
 
-from client.instagram import login
+from decouple import config
+from pymongo import MongoClient
+
+from client_instagram import login
 from commands import update_users, search_feed_media, search_stories
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-c", "--command", help="Choose command to run")
-
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--command", help="Choose command to run")
+
     api = login()
-    db_users = None
-    db_media = None
+    client = MongoClient(
+            host=config('MONGO_ADDR'),
+            port=config('MONGO_PORT', cast=int),
+    )
+    db_users = client.shoes.users
+    db_media = client.shoes.media
 
-    args = parser.parse_args()
-    if args.command == 'update-followers':
+    arguments = parser.parse_args()
+    if arguments.command == 'update-followers':
         followers = api.getTotalFollowers(api.username_id)
-        update_users(api, db_users, orgin='follower')
+        update_users(db_users, followers, origin='follower')
 
-    elif args.command == 'update-followings':
+    elif arguments.command == 'update-followings':
         followings = api.getTotalFollowings(api.username_id)
-        update_users(api, db_users, orgin='following')
+        update_users(db_users, followings, origin='following')
 
-    elif args.command == 'update-media':
+    elif arguments.command == 'update-media':
         followers = db_users.find(
                 {'origin': 'follower'}
         ).limit(50).sort("last_visited")
@@ -33,14 +39,14 @@ if __name__ == "__main__":
                 {'origin': 'following'}
         ).limit(50).sort("last_visited")
 
-        users = followers + followings
+        users = list(followers) + list(followings)
         for user in users:
             search_feed_media(api, db_media, user)
             search_stories(api, db_media, user)
 
             now = datetime.utcnow().timestamp()
             user['last_visited'] = now
-            db_users.upddate_one(
+            db_users.update_one(
                     {'pk': user['pk'], 'origin': user['origin']},
                     {"$set": user}, upsert=True
             )
