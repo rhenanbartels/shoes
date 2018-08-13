ee0f439d2e1e9d9d921546e12d91a111f278586e
Rhenan Bartels <rhenan.bartels@gmail.com>
2018-08-13 18:30:57 -0300

    Correct whitenoise

--- shoes/settings.py
+++ shoes/settings.py
@@ -30,7 +30,7 @@ DEBUG = config('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Application definition
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
